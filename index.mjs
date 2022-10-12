ae8d2eb473df3b24919f80bcfcda563e91c07fa0
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-10-12 13:53:22 +0900

    fix: include user-agent, referer in requests

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import urls from './urls.json' assert { type: 'json' };
 
 const zoomUrlRegex = /https:\/\/(.*\.)?(zoom.us|zoomgov.com)\/rec\/share\/.+\?pwd=.+/;
+const zoomMp4UrlRegex = /https:\/\/ssrweb\..+\/(.+\.mp4)[^'"]+/g;
 const setCookieRegex = /([^,= ]+=[^,;]+);? *([^,= ]+(=(Mon,|Tue,|Wed,|Thu,|Fri,|Sat,|Sun,)?[^,;]+)?;? *)*/g;
 
 if (typeof (fetch) === 'undefined')
@@ -15,31 +16,43 @@ for (const url of urls) {
 };
 
 for await (const url of urls) {
+	const headers = new Headers({
+		// Chrome 106 on Windows 11
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+	});
+
 	// Redirect response with Set-Cookie header
-	const initialResponse = await fetch(url);
+	const initialResponse = await fetch(url, { headers });
 
 	if (!initialResponse.ok || !initialResponse.redirected || !initialResponse.headers.has('set-cookie'))
 		throw new Error(`Initial fetch has failed. (${url})`);
 
 	// Node.js Fetch API merges Set-Cookie headers into a single string
 	const setCookieString = initialResponse.headers.get('set-cookie');
-
-	const cookie = [...setCookieString.matchAll(setCookieRegex)]
+	const cookieString = [...setCookieString.matchAll(setCookieRegex)]
 		.map(([, group1]) => (group1))
 		.join('; ');
 
+	headers.append('Cookie', cookieString);
+
 	// Re-request the video download page with authentication cookie (_zm_ssid)
-	const downloadPageResponse = await fetch(url, { headers: { cookie } });
+	// Returns different response based on the User-Agent (e.g. global data)
+	const downloadPageResponse = await fetch(url, { headers });
 
 	if (!downloadPageResponse.ok)
 		throw new Error(`Download page fetch has failed. (${url})`);
 
 	const downloadPageHtml = await downloadPageResponse.text();
-	const videoUrlMatch = downloadPageHtml.match(/src="(.+\.mp4)/);
+	const videoUrlMatches = [...downloadPageHtml.matchAll(zoomMp4UrlRegex)]
+		.map(([url, filename]) => ({ url, filename }));
 
-	if (videoUrlMatch === null)
+	if (!videoUrlMatches.length)
 		throw new Error(`Video URL is not found. (${url})`);
 
-	const videoUrl = videoUrlMatch[1];
-	console.log(videoUrl);
+	headers.append('Referer', 'https://zoom.us/');
+
+	for await (const { url, filename } of videoUrlMatches) {
+		const response = await fetch(url, { headers });
+		console.log(response.ok, filename);
+	};
 };
