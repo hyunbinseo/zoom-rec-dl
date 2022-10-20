9aee44abe4ac3ac319b57bba575c98b01d661821
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-10-20 16:32:30 +0900

    feat: skip failed downloads, don't throw

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { createWriteStream, existsSync, mkdirSync, renameSync } from 'node:fs';
+import { createWriteStream, existsSync, mkdirSync, renameSync, writeFileSync } from 'node:fs';
 import { Readable } from 'node:stream';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -15,31 +15,33 @@ const regex = {
 	setCookie: /([^,= ]+=[^,;]+);? *(?:[^,= ]+(?:=(?:Mon,|Tue,|Wed,|Thu,|Fri,|Sat,|Sun,)?[^,;]+)?;? *)*/g
 };
 
+// Runtime Validation
+
+if (typeof (fetch) === 'undefined') throw new Error('Fetch API is not supported. Use Node.js v18 or later.');
+
 // Setting Validation
 
 const { download } = settings;
 
-if (typeof download.folder !== 'string' || !/^[a-z]+$/.test(download.folder))
-	throw new Error(`Download folder is not valid. (${download.folder})`);
+if (typeof download.folder !== 'string') throw new Error('Download folder should be a string.');
+if (!/^[a-z]+$/.test(download.folder)) throw new Error(`Download folder is not valid. (${download.folder})`);
 
 const downloadFolder = `${__dirname}/${download.folder}`;
 
-// Runtime Validation
-
-if (typeof (fetch) === 'undefined')
-	throw new Error('Fetch API is not supported. Use Node.js v18 or later.');
-
 // Zoom URL Validation
 
-if (!(Array.isArray(urls) && urls.length))
-	throw new Error('Zoom URL is not found.');
+if (!Array.isArray(urls) || !urls.length) throw new Error('Zoom URL is not found.');
 
 for (const url of urls) {
-	if (typeof url !== 'string' || !regex.zoomShare.test(url) || url === 'https://zoom.us/rec/share/unique-id?pwd=password')
-		throw new Error(`Zoom URL is not valid. (${url})`);
+	if (typeof url !== 'string') throw new Error('Zoom URL should be a string.');
+	if (!regex.zoomShare.test(url)) throw new Error(`Zoom URL is not valid. (${url})`);
+	if (url === 'https://zoom.us/rec/share/unique-id?pwd=password') throw new Error('Sample Zoom URL is found. Check the urls.json file.');
 };
 
 // Download Video Files
+
+const failedShareUrls = [];
+const failedVideoUrls = [];
 
 for await (const url of urls) {
 	const headers = new Headers({
@@ -50,8 +52,11 @@ for await (const url of urls) {
 	// Redirect response with Set-Cookie header
 	const initialResponse = await fetch(url, { headers });
 
-	if (!initialResponse.ok || !initialResponse.redirected || !initialResponse.headers.has('set-cookie'))
-		throw new Error(`Initial fetch has failed. (${url})`);
+	if (!initialResponse.ok || !initialResponse.redirected) {
+		console.error(`Initial fetch has failed. (${url})`);
+		failedShareUrls.push(url);
+		continue;
+	};
 
 	// Node.js Fetch API merges Set-Cookie headers into a single string
 	const setCookieString = initialResponse.headers.get('set-cookie') || '';
@@ -65,25 +70,35 @@ for await (const url of urls) {
 	// Returns different response based on the User-Agent (e.g. global data)
 	const downloadPageResponse = await fetch(url, { headers });
 
-	if (!downloadPageResponse.ok)
-		throw new Error(`Download page fetch has failed. (${url})`);
+	if (!downloadPageResponse.ok) {
+		console.error(`Download page fetch has failed. (${url})`);
+		failedShareUrls.push(url);
+		continue;
+	};
 
 	const downloadPageHtml = await downloadPageResponse.text();
 	const videoUrlMatches = [...downloadPageHtml.matchAll(regex.zoomVideo)]
 		.map(([url, filename]) => ({ url, filename }));
 
-	if (!videoUrlMatches.length)
-		throw new Error(`Video URL is not found. (${url})`);
+	if (!videoUrlMatches.length) {
+		console.error(`Video URL is not found. (${url})`);
+		failedShareUrls.push(url);
+		continue;
+	};
 
 	headers.append('Referer', 'https://zoom.us/');
 
+	console.log(Date.now());
 	console.log(`Downloading video file(s). (${url})`);
 
 	for await (const { url, filename } of videoUrlMatches) {
 		const response = await fetch(url, { headers });
 
-		if (!response.ok)
-			throw new Error(`Video file fetch has failed. (${filename})`);
+		if (!response.ok) {
+			console.error(`Video file fetch has failed. (${filename})`);
+			failedVideoUrls.push(url);
+			continue;
+		};
 
 		if (!existsSync(downloadFolder)) mkdirSync(downloadFolder);
 
@@ -103,7 +118,8 @@ for await (const url of urls) {
 				resolve();
 			});
 			readable.on('error', (error) => {
-				console.log(`Failed to download file. (${filename})`);
+				console.error(`Failed to download file. (${filename})`);
+				failedVideoUrls.push(url);
 				reject(error);
 			});
 		});
@@ -113,3 +129,16 @@ for await (const url of urls) {
 };
 
 console.log('Download completed.');
+
+if (failedShareUrls.length || failedVideoUrls.length) {
+	const filename = `${Date.now()}.txt`;
+	const log = [
+		`Failed Zoom Share URL (${failedShareUrls.length})`,
+		...failedShareUrls,
+		'',
+		`Failed Zoom Video URL (${failedVideoUrls.length})`,
+		...failedVideoUrls
+	].join('\n');
+	writeFileSync(`${__dirname}/${filename}`, log);
+	console.error(`Failed attempts found. Check ${filename} for more information.`);
+};
